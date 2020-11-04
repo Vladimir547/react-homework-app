ebfb8588ae8f2dcca2f9d5a27d3249c25c2f1051
Vladimir547 <germes547@gmail.ru>
2020-11-04 19:27:40 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <App default={10}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
